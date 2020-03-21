54589e1bfacbf02f9b235a96861b3cb0c74172c0
jotadaxter <jotadaxter@gmail.com>
2020-03-21 19:52:01 +0000

    Revert "test try"
    This reverts commit 90793030d898134cd3dbb75e15af553496d1325e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule, routingComponents, routingPageComponents } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './utilities/header/header.component';
 import { NavbarComponent } from './utilities/navbar/navbar.component';
 import { FooterComponent } from './utilities/footer/footer.component';
-import { AboutComponent } from './utilities/about/about.component';
-import { ContactComponent } from './utilities/contact/contact.component';
-import { ProjectsComponent } from './utilities/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    routingPageComponents,
     HeaderComponent,
     NavbarComponent,
-    FooterComponent,
-    AboutComponent,
-    ContactComponent,
-    ProjectsComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
