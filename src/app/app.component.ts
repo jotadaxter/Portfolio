98010e625004435d2eba681bdc0239e4ae6783d2
jotadaxter <jotadaxter@gmail.com>
2020-10-17 13:11:01 +0100

    new try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {enableProdMode} from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +8,14 @@ import {enableProdMode} from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio JACMS';
+
+  constructor(private router: Router) {
+    const path = localStorage.getItem('path');
+    if (path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+  }
 }
 
 enableProdMode();
